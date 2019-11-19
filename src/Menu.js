4f707696e81af596cc52687264f85a6fe1891dc4
Evgeny Leukhin <eugeny.leukhin@stellarsolvers.com>
2019-11-19 11:29:58 +0600

    Add empty pages

--- src/Menu.js
+++ src/Menu.js
@@ -1,179 +1,50 @@
 const Menu = [
-    {
-        id: 0,
-        name: 'Jobs',
-        path: '/jobs',
-        icon: 'img/icons/clipboard.svg'
-    },
-    {
-        id: 1,
-        name: 'Companies',
-        path: '/companies',
-        icon: 'img/icons/aperture.svg'
-    },
-    {
-        id: 2,
-        name: 'Employers',
-        path: '/employers',
-        icon: 'img/icons/navicon.svg'
-    },
-    {
-        id: 3,
-        name: 'Users',
-        path: '/users',
-        icon: 'img/icons/person-stalker.svg'
-    },
-    {
-        id: 4,
-        name: 'Skills',
-        path: '/skills',
-        icon: 'img/icons/grid.svg'
-    },
-    {
-        id: 5,
-        name: 'Roles',
-        path: '/roles',
-        icon: 'img/icons/radio-waves.svg'
-    },
-    {
-        id: 6,
-        name: 'Plans',
-        path: '/plans',
-        icon: 'img/icons/radio-waves.svg'
-    },
-    {
-        id: 7,
-        name: 'Skills',
-        path: '/skills',
-        icon: 'img/icons/radio-waves.svg'
-    },
-    {
-        id: 8,
-        name: 'Charts',
-        path: '/charts',
-        icon: 'img/icons/connection-bars.svg'
-    },
-];
-
-
-const Menu2 = [{
-        name: 'Dashboard',
-        path: '/dashboard',
-        icon: 'img/icons/aperture.svg',
-        label: { value: 1, classNme: 'bg-success' }
-    }, {
-        name: 'Cards',
-        path: '/cards',
-        icon: 'img/icons/radio-waves.svg'
-    }, {
-        name: 'Charts',
-        icon: 'img/icons/connection-bars.svg',
-        submenu: [{
-            name: 'Flot',
-            path: '/charts/flot'
-        }, {
-            name: 'Radial',
-            path: '/charts/radial'
-        }, {
-            name: 'ChartJS',
-            path: '/charts/chartjs'
-        }]
-    }, {
-        name: 'Forms',
-        icon: 'img/icons/clipboard.svg',
-        submenu: [{
-            name: 'Classic',
-            path: '/forms/classic'
-        }, {
-            name: 'Validation',
-            path: '/forms/validation'
-        }, {
-            name: 'Advanced',
-            path: '/forms/advanced'
-        }, {
-            name: 'Material',
-            path: '/forms/material'
-        }, {
-            name: 'Editors',
-            path: '/forms/editors'
-        }, {
-            name: 'Dropzone',
-            path: '/forms/dropzone'
-        }]
-    }, {
-        name: 'Tables',
-        icon: 'img/icons/navicon.svg',
-        submenu: [
-            { name: 'Classic', path: '/tables/classic' },
-            { name: 'Datatable', path: '/tables/datatable' },
-            { name: 'DataGrid', path: '/tables/datagrid' }
-        ]
-    }, {
-        name: 'Layouts',
-        icon: 'img/icons/grid.svg',
-        submenu: [
-            { name: 'Columns', path: '/layouts/columns' },
-            { name: 'Overlap', path: '/layouts/overlap' },
-            { name: 'Boxed', path: '/layouts/boxed' },
-            { name: 'Tabs', path: '/layouts/tabs' },
-            { name: 'Containers', path: '/layouts/containers' },
-        ]
-    }, {
-        name: 'Elements',
-        icon: 'img/icons/levels.svg',
-        submenu: [
-            { name: 'Colors', path: '/elements/colors' },
-            { name: 'Whiteframes', path: '/elements/whiteframes' },
-            { name: 'Lists', path: '/elements/lists' },
-            { name: 'Bootstrap', path: '/elements/bootstrap' },
-            { name: 'Buttons', path: '/elements/buttons' },
-            { name: 'Sweet-alert', path: '/elements/sweetalert' },
-            { name: 'Spinners', path: '/elements/spinners' },
-            { name: 'Toastify', path: '/elements/toastify' },
-            { name: 'Grid', path: '/elements/grid' },
-            { name: 'Grid Masonry', path: '/elements/gridmasonry' },
-            { name: 'Typography', path: '/elements/typography' },
-            { name: 'Icons', path: '/elements/icons' },
-            { name: 'Utilities', path: '/elements/utilities' }
-        ]
-    }, {
-        name: 'Maps',
-        icon: 'img/icons/planet.svg',
-        submenu: [
-            { name: 'Google Maps Full', path: '/maps/googlefull' },
-            { name: 'Google Maps', path: '/maps/google' },
-            { name: 'Vector Maps', path: '/maps/vector' },
-            { name: 'Datamaps', path: '/maps/datamaps' }
-        ]
-    }, {
-        name: 'Pages',
-        icon: 'img/icons/ios-browsers.svg',
-        submenu: [
-            { name: 'Timeline', path: '/pages/timeline' },
-            { name: 'Invoice', path: '/pages/invoice' },
-            { name: 'Pricing', path: '/pages/pricing' },
-            { name: 'Contacts', path: '/pages/contacts' },
-            { name: 'FAQ', path: '/pages/faq' },
-            { name: 'Projects', path: '/pages/projects' },
-            { name: 'Blog', path: '/pages/blog' },
-            { name: 'Article', path: '/pages/article' },
-            { name: 'Profile', path: '/pages/profile' },
-            { name: 'Gallery', path: '/pages/gallery' },
-            { name: 'Wall', path: '/pages/wall' },
-            { name: 'Search', path: '/pages/search' },
-            { name: 'Messages Board', path: '/pages/messagesboard' }
-        ]
-    }, {
-        name: 'User',
-        icon: 'img/icons/person-stalker.svg',
-        submenu: [
-            { name: 'Login', path: '/login' },
-            { name: 'Signup', path: '/signup' },
-            { name: 'Lock', path: '/lock' },
-            { name: 'Recover', path: '/recover' }
-        ]
-    },
-
+  {
+    id: 0,
+    name: 'Users',
+    icon: 'img/icons/person-stalker.svg',
+    path: '/users'
+  },
+  {
+    id: 1,
+    name: 'Companies',
+    icon: 'img/icons/ios-browsers.svg',
+    path: '/companies'
+  },
+  {
+    id: 2,
+    name: 'Charts',
+    icon: 'img/icons/connection-bars.svg',
+    submenu: [
+      { name: 'Talents',   path: '/talents' },
+      { name: 'Employers', path: '/employers' },
+      { name: 'Filters',   path: '/filters' }
+    ],
+  },
+  {
+    id: 3,
+    name: 'Jobs',
+    icon: 'img/icons/navicon.svg',
+    path: '/jobs'
+  },
+  {
+    id: 4,
+    name: 'Skills',
+    icon: 'img/icons/navicon.svg',
+    path: '/skills'
+  },
+  {
+    id: 5,
+    name: 'Roles',
+    icon: 'img/icons/navicon.svg',
+    path: '/roles'
+  },
+  {
+    id: 6,
+    name: 'Plans',
+    icon: 'img/icons/navicon.svg',
+    path: '/plans'
+  },
 ];
 
 export default Menu;
--- src/Routes.js
+++ src/Routes.js
@@ -3,35 +3,21 @@ import { withRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 /* loader component for Suspense */
-import PageLoader from './components/Common/PageLoader';
-
-/* eslint-disable-next-line */
-import Core from './components/Core/Core';
-/* eslint-disable-next-line */
-import Bootstrap from './components/Bootstrap/Bootstrap';
-/* eslint-disable-next-line */
-import Common from './components/Common/Common';
-/* eslint-disable-next-line */
-import Colors from './components/Colors/Colors';
-/* eslint-disable-next-line */
+import PageLoader  from './components/Common/PageLoader';
+import Core        from './components/Core/Core';
+import Bootstrap   from './components/Bootstrap/Bootstrap';
+import Common      from './components/Common/Common';
+import Colors      from './components/Colors/Colors';
 import FloatButton from './components/FloatButton/FloatButton';
-/* eslint-disable-next-line */
-import Utils from './components/Utils/Utils';
+import Utils       from './components/Utils/Utils';
 
 import User from './views/User/User';
 
 /* Used to render a lazy component with react-router */
 const waitFor = Tag => props => <Tag {...props}/>;
 
-const Dashboard = lazy(() => import('./views/Dashboard/Dashboard'));
-const Cards = lazy(() => import('./views/Cards/Cards'));
-const Charts = lazy(() => import('./views/Charts/Charts'));
-const Forms = lazy(() => import('./views/Forms/Forms'));
-const Elements = lazy(() => import('./views/Elements/Elements'));
-const Tables = lazy(() => import('./views/Tables/Tables'));
-const Layouts = lazy(() => import('./views/Layouts/Layouts'));
-const Maps = lazy(() => import('./views/Maps/Maps'));
-const Pages = lazy(() => import('./views/Pages/Pages'));
+const Users     = lazy(() => import('./views/Pages/Users'));
+const Companies = lazy(() => import('./views/Pages/Companies'));
 
 
 // List of routes that uses the page layout
@@ -71,17 +57,10 @@ const Routes = ({ location }) => {
                         <div>
                             <Suspense fallback={<PageLoader/>}>
                                 <Switch location={location}>
-                                    <Route path="/dashboard" component={waitFor(Dashboard)}/>
-                                    <Route path="/cards" component={waitFor(Cards)}/>
-                                    <Route path="/charts" component={waitFor(Charts)}/>
-                                    <Route path="/forms" component={waitFor(Forms)}/>
-                                    <Route path="/tables" component={waitFor(Tables)}/>
-                                    <Route path="/layouts" component={waitFor(Layouts)}/>
-                                    <Route path="/elements" component={waitFor(Elements)}/>
-                                    <Route path="/maps" component={waitFor(Maps)}/>
-                                    <Route path="/pages" component={waitFor(Pages)}/>
-    
-                                    <Redirect to="/dashboard"/>
+                                    <Route path="/users" component={waitFor(Users)} />
+                                    <Route path="/companies" component={waitFor(Companies)} />
+
+                                    <Redirect to="/users"/>
                                 </Switch>
                             </Suspense>
                         </div>
