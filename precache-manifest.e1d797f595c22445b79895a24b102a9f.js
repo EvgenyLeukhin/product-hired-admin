f6e11f99ad36332c8f3ca40de7cae68b5f5b36b8
Evgeny Leukhin <eugeny.leukhin@stellarsolvers.com>
2019-11-19 13:26:26 +0600

    Updates

---
+++ precache-manifest.e1d797f595c22445b79895a24b102a9f.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "a89666e2e77ae492aea9",
+    "url": "/product-hired-admin/static/js/9.a89666e2.chunk.js"
+  },
+  {
+    "revision": "6fc241fb223b70fdf2f5",
+    "url": "/product-hired-admin/static/css/main.b461e1b6.chunk.css"
+  },
+  {
+    "revision": "f875bee402ca2cf8f843",
+    "url": "/product-hired-admin/static/js/1.f875bee4.chunk.js"
+  },
+  {
+    "revision": "6c7570ee2a6df8b15c26",
+    "url": "/product-hired-admin/static/js/2.6c7570ee.chunk.js"
+  },
+  {
+    "revision": "12a3036da0a41b1d246f",
+    "url": "/product-hired-admin/static/js/3.12a3036d.chunk.js"
+  },
+  {
+    "revision": "0b6900a102bd19252b4e",
+    "url": "/product-hired-admin/static/js/4.0b6900a1.chunk.js"
+  },
+  {
+    "revision": "2c92a9bfba34266f28ac",
+    "url": "/product-hired-admin/static/js/5.2c92a9bf.chunk.js"
+  },
+  {
+    "revision": "e393f21025264274cbf2",
+    "url": "/product-hired-admin/static/js/6.e393f210.chunk.js"
+  },
+  {
+    "revision": "38ca3db038d783b884b8",
+    "url": "/product-hired-admin/static/js/7.38ca3db0.chunk.js"
+  },
+  {
+    "revision": "fe1e467277746f83a713",
+    "url": "/product-hired-admin/static/js/8.fe1e4672.chunk.js"
+  },
+  {
+    "revision": "6fc241fb223b70fdf2f5",
+    "url": "/product-hired-admin/static/js/main.6fc241fb.chunk.js"
+  },
+  {
+    "revision": "28df6ee7b407fd8a14b40bc01f4fd3ae",
+    "url": "/product-hired-admin/static/media/ionicons.28df6ee7.svg"
+  },
+  {
+    "revision": "d7bc03de24ff2a5c7771",
+    "url": "/product-hired-admin/static/js/10.d7bc03de.chunk.js"
+  },
+  {
+    "revision": "dd4781d1acc57ba4c4808d1b44301201",
+    "url": "/product-hired-admin/static/media/ionicons.dd4781d1.ttf"
+  },
+  {
+    "revision": "58b61a4b98dc3eb3ffa3",
+    "url": "/product-hired-admin/static/js/11.58b61a4b.chunk.js"
+  },
+  {
+    "revision": "733b7c8cad185c1a985e",
+    "url": "/product-hired-admin/static/js/runtime~main.733b7c8c.js"
+  },
+  {
+    "revision": "2c159d0d05473040b53ec79df8797d32",
+    "url": "/product-hired-admin/static/media/ionicons.2c159d0d.woff"
+  },
+  {
+    "revision": "19e65b89cee273a249fba4c09b951b74",
+    "url": "/product-hired-admin/static/media/ionicons.19e65b89.eot"
+  },
+  {
+    "revision": "58b61a4b98dc3eb3ffa3",
+    "url": "/product-hired-admin/static/css/11.6e0c3594.chunk.css"
+  },
+  {
+    "revision": "d7bc03de24ff2a5c7771",
+    "url": "/product-hired-admin/static/css/10.e7ed8484.chunk.css"
+  },
+  {
+    "revision": "4a90d553466a3c88ebeb6e6c91a31c84",
+    "url": "/product-hired-admin/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9107466cff0012aa76c9ead501ea7f83.js"
+  "/product-hired-admin/precache-manifest.e1d797f595c22445b79895a24b102a9f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/product-hired-admin/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
