721bc8fc57534f34e9d9ccce98f7a60023ae1ba9
Evgeny Leukhin <eugeny.leukhin@stellarsolvers.com>
2019-11-22 00:06:23 +0600

    Users

--- src/views/Pages/Users.js
+++ src/views/Pages/Users.js
@@ -1,18 +1,79 @@
-import React, { Component } from "react";
+import React from "react";
+import ReactTable from "react-table";
+
+import matchSorter from 'match-sorter';
+
+import axios from 'axios';
+import API_URL from '../../consts/apiUrl';
+
 import { withHeaderTitle } from '../../components/Header/HeaderTitle';
 
-class Users extends Component {
-    componentWillMount() {
-        this.props.setHeaderTitle('Users');
-    }
 
-    render() {
-        return (
-            <section className="section-container">
-              Users List - TO
-            </section>
-        );
-    }
+class Users extends React.Component {
+  state = {
+    loading: false,
+    users: []
+  }
+
+  UNSAFE_componentWillMount() {
+    this.props.setHeaderTitle('Users');
+  }
+
+  componentDidMount() {
+    const userToken = localStorage.getItem('ph-admin-token');
+
+    axios.get(`${API_URL}/api/api/users`, {
+        headers: { Authorization: userToken }
+    })
+        .then(this.setState({ loading: true }))
+
+        .then(res => {
+            this.setState({ 
+              users: res.data,
+              loading: false,
+            });
+        })
+  }
+
+  render() {
+    const { users } = this.state;
+
+    const columns = [
+      { 
+        Header: 'ID',
+        accessor: 'id',
+        width: 60,
+        Cell: ({ original }) => {
+          return (
+            <div style={{ textAlign: 'right' }}>
+              <span>{original.id}</span>
+            </div>
+          );
+        }
+      }, 
+      { 
+        Header: 'Name', 
+        accessor: 'name',
+        id: 'name',
+        accessor: d => d.name,
+        filterMethod: (filter, rows) => matchSorter(rows, filter.value, { keys: ['name'] }),
+        filterAll: true
+      }
+    ];
+    
+    return (
+      <div>
+        <ReactTable
+          className="-striped -highlight"
+          data={users}
+          columns={columns}
+          filterable={true}
+          resizable={true}
+        />
+
+      </div>
+    );
+  }
 }
 
 export default withHeaderTitle(Users);
