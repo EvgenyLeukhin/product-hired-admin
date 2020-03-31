b503c5e516226df9a776bad393c04ca42648549a
Evgeny Leukhin <eugeny.leukhin@stellarsolvers.com>
2020-03-31 09:40:07 +0600

    Comment 404 and redirect

--- src/ph-admin/routes/index.js
+++ src/ph-admin/routes/index.js
@@ -76,10 +76,10 @@ const Routes = ({ location }) => {
       <Route path="/plans/:id"     component={PlansDetail} />
       <Route path="/plans"         component={Plans} />
 
-      <Route path="*"              component={() => <h2>404 - Page not found</h2>} />
+      {/* <Route path="*"              component={() => <h2>404 - Page not found</h2>} /> */}
 
       {/* if wrong route */}
-      <Redirect to="/companies" />
+      {/* <Redirect to="/companies" /> */}
     </Core>
   );
 }
