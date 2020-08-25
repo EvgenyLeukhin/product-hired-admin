832cf9266ee15c345823c0f2f004d60910024c89
Evgeny Leukhin <eugenel@effective.band>
2020-08-25 15:07:33 +0600

    create new branch campaings

--- src/ph-admin/api/apiUrl.js
+++ src/ph-admin/api/apiUrl.js
@@ -1,4 +1,5 @@
-const API_URL = 'https://stage1.producthired.com';
+// const API_URL = 'https://stage1.producthired.com';  // test API
+const API_URL = 'https://producthired.com';         // prod API
 const subUrl = 'api/api';
 
 export { API_URL, subUrl };
