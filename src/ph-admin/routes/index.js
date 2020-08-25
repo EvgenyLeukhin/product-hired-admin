993f7a874c10ffa316a4219528c5a3e971e525d1
Evgeny Leukhin <eugenel@effective.band>
2020-08-25 16:02:14 +0600

    Add lazy loading for routes

--- src/ph-admin/routes/index.js
+++ src/ph-admin/routes/index.js
@@ -1,33 +1,61 @@
 import React from 'react';
-import { withRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { withRouter, Switch, Route, Redirect, lazy } from 'react-router-dom';
 
 /* loader component for Suspense */
-import Core        from '../../components/Core/Core';
-import Profile     from './UserProfile/';
+const Core            = lazy(() => import('../../components/Core/Core'));
+const Profile         = lazy(() => import('./UserProfile/'));
 
-import Companies       from './Companies/index.jsx';
-import CompaniesDetail from './Companies/detail.jsx';
+const Companies       = lazy(() => import('./Companies/index.jsx'));
+const CompaniesDetail = lazy(() => import('./Companies/detail.jsx'));
 
-import Users        from './Users/index.jsx';
-import UsersDetail  from './Users/detail.jsx';
+const Users           = lazy(() => import('./Users/index.jsx'));
+const UsersDetail     = lazy(() => import('./Users/detail.jsx'));
 
-import Jobs         from './Jobs/index.jsx';
-import JobsDetail   from './Jobs/detail.jsx';
+const Jobs            = lazy(() => import('./Jobs/index.jsx'));
+const JobsDetail      = lazy(() => import('./Jobs/detail.jsx'));
 
-import Skills       from './Skills/index.jsx';
-import SkillsDetail from './Skills/detail.jsx';
+const Skills          = lazy(() => import('./Skills/index.jsx'));
+const SkillsDetail    = lazy(() => import('./Skills/detail.jsx'));
 
-import Roles        from './Roles/index.jsx';
-import RolesDetail  from './Roles/detail.jsx';
+const Roles           = lazy(() => import('./Roles/index.jsx'));
+const RolesDetail     = lazy(() => import('./Roles/detail.jsx'));
 
-import Plans        from './Plans/index.jsx';
-import PlansDetail  from './Plans/detail.jsx';
+const Plans           = lazy(() => import('./Plans/index.jsx'));
+const PlansDetail     = lazy(() => import('./Plans/detail.jsx'));
 
-import Campaings       from './Campaings/index.jsx';
-import CampaingsDetail from './Campaings/detail.jsx';
+const Campaings       = lazy(() => import('./Campaings/index.jsx'));
+const CampaingsDetail = lazy(() => import('./Campaings/detail.jsx'));
 
-import PagesForAdvertising       from './PagesForAdvertising/index.jsx';
-import PagesForAdvertisingDetail from './PagesForAdvertising/detail.jsx';
+const PagesForAdvertising       = lazy(() => import('./PagesForAdvertising/index.jsx'));
+const PagesForAdvertisingDetail = lazy(() => import('./PagesForAdvertising/detail.jsx'));
+
+
+// import Core        from '../../components/Core/Core';
+// import Profile     from './UserProfile/';
+
+// import Companies       from './Companies/index.jsx';
+// import CompaniesDetail from './Companies/detail.jsx';
+
+// import Users        from './Users/index.jsx';
+// import UsersDetail  from './Users/detail.jsx';
+
+// import Jobs         from './Jobs/index.jsx';
+// import JobsDetail   from './Jobs/detail.jsx';
+
+// import Skills       from './Skills/index.jsx';
+// import SkillsDetail from './Skills/detail.jsx';
+
+// import Roles        from './Roles/index.jsx';
+// import RolesDetail  from './Roles/detail.jsx';
+
+// import Plans        from './Plans/index.jsx';
+// import PlansDetail  from './Plans/detail.jsx';
+
+// import Campaings       from './Campaings/index.jsx';
+// import CampaingsDetail from './Campaings/detail.jsx';
+
+// import PagesForAdvertising       from './PagesForAdvertising/index.jsx';
+// import PagesForAdvertisingDetail from './PagesForAdvertising/detail.jsx';
 
 // common styles //
 import '../../components/Bootstrap/Bootstrap';
@@ -36,7 +64,8 @@ import '../../components/Colors/Colors';
 import '../../components/FloatButton/FloatButton';
 import '../../components/Utils/Utils';
 
-import User from '../../views/User/User';
+// import User from '../../views/User/User';
+const User = lazy(() => import('../../views/User/User'));
 
 
 /* Used to render a lazy component with react-router */
@@ -89,7 +118,7 @@ const Routes = ({ location }) => {
       <Route path="/pages-for-advertising/:id" component={PagesForAdvertisingDetail} />
       <Route path="/pages-for-advertising"     component={PagesForAdvertising} />
 
-      <Route path="/profile"       component={Profile} />
+      <Route path="/profile" component={Profile} />
 
       {/* <Route path="*"              component={() => <h2>404 - Page not found</h2>} /> */}
 
